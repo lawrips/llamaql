d907628c332b8ea04600699b651368732e718062
lripsher <lawrence@ripsher.com>
2024-09-15 16:37:53 -0700

    build bug fixes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useQueryState } from '../hooks/useQueryState';
 import QueryInput from '../components/QueryInput';
@@ -11,25 +11,22 @@ import ResultPanel from '../components/ResultPanel';
 import ActionButtons from '../components/ActionButtons';
 import LoadingOverlay from '../components/LoadingOverlay';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 import ModalDialog from '@/components/ModalDialog'; 
 import TermsModal from '@/components/TermsModal';
 
-export default function Home() {
+function HomeContent({ appName }) {
     const router = useRouter();
-    const searchParams = useSearchParams();
-    let appName = searchParams.get('app');
 
     useEffect(() => {
         if (!appName) {
             router.push('/upload');
         }
-    }, [searchParams, router]);
+    }, [appName, router]);
 
     if (!appName) {
         return null;
-      }
-    
+    }
+
     const {
         userQuery,
         setUserQuery,
@@ -77,15 +74,13 @@ export default function Home() {
         handleDialogClose,
         handleSaveInstructions
     } = useQueryState(appName);    
-    
 
     return (
         <div style={{ padding: "10px" }}>
             <div className="col-span-2">
                 <h3 className="text-xl font-bold mb-2"><a href="/upload">llamaql (v0.1)</a></h3>
             </div>
-            <div className="flex w-full gap-4 p-4" >
-
+            <div className="flex w-full gap-4 p-4">
 
                 <QueryInput
                     userQuery={userQuery}
@@ -108,7 +103,7 @@ export default function Home() {
                 />
             </div>
             <div className="flex w-full gap-4 pl-4 pr-4 pb-4">
-            <ModelSelector
+                <ModelSelector
                     selectedModel={selectedModel}
                     setSelectedModel={setSelectedModel}
                     models={models}
@@ -120,7 +115,6 @@ export default function Home() {
                 <div>
                     <button onClick={() => handleQuery(true)}>Requery</button>
                 </div>
-
             </div>
 
             <InstructionPanel
@@ -157,7 +151,7 @@ export default function Home() {
                 fileInputRef={fileInputRef}
                 handleFileChange={handleFileChange}
             />
-            <br/>
+            <br />
             By using this research prototype, you agree to the <a target="_blank" href="/terms.html">Terms and Conditions</a>.
 
             <LoadingOverlay loading={loading} />
@@ -166,10 +160,19 @@ export default function Home() {
                 open={dialogOpen}
                 handleDialogClose={handleDialogClose}
                 title="Success"
-                content={`Action completed successfuly!`}
+                content={`Action completed successfully!`}
             />
-
-
         </div>
+    );
+}
+
+export default function Home() {
+    const searchParams = useSearchParams();
+    const appName = searchParams.get('app');
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <HomeContent appName={appName} />
+        </Suspense>
     );
 }

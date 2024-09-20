e167f6698a1ebc95a8e9e4bcbfaeacbd45c1daf2
lripsher <lawrence@ripsher.com>
2024-09-19 23:38:28 -0700

    text change

--- components/AnnotationInput.js
+++ components/AnnotationInput.js
@@ -7,13 +7,14 @@ const AnnotationInput = ({ annotation, setAnnotation, focusedInput, setFocusedIn
     <input
       className="w-full p-2 border rounded"
       autocomplete="off"
+      id="customField22"
       style={{ width: '100%', boxSizing: 'border-box' }}
       value={annotation}
       onChange={(e) => setAnnotation(e.target.value)}
       onFocus={() => setFocusedInput('annotation')}
       onBlur={() => setFocusedInput(null)}
       type="text"
-      placeholder="Instructions (add hints or additional details if helpful)"
+      placeholder="Hints (optional if it helps AI)"
     />
   </div>
   );
